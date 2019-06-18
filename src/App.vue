d8769b53a998fbfc9f0afcdeda6534df80b6de60
abyu404 <abyu629@gmail.com>
2019-06-18 23:55:53 +0900

    6/18

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,12 @@
 <template>
   <div id="app">
-   <Todos v-bind:todos="todos" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Todos from './components/Todos';
-
-
-export default {
-  name: 'app',
-  components: {
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id:1,
-           title:"Todo One",
-           completed: false
-        },
-        {
-          id:2,
-           title:"Todo Two",
-           completed: true
-        },
-        {
-          id:3,
-           title:"Todo Three",
-           completed: false
-        },
-      ]
-    }
-  }
-}
-</script>
 
 
 <style>
